023c9a59baa20ad0cf30673e9a61a7cb422119bf
Vuong Ho <hochivuong2002@gmail.com>
2024-03-06 02:16:37 -0500

    Update utils

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,3 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-import logging
-import asyncio
-
 
 def print_bytes(bs: bytes) -> str:
     return f"{int.from_bytes(bs)} (0x{bs.hex()})"
@@ -21,62 +15,3 @@ def convert_packet_id_to_int(packet_id: str) -> int:
     elif packet_id.startswith("0x"):
         # If it starts with "0x", treat it as hex
         return int(packet_id, 16)
-
-def verify_rsa_signature(
-        data: bytes,
-        signature: bytes,
-        modulus: int,
-        exponent:
-        int
-    ) -> tuple[bool, str, str]:
-    """
-    Verify an RSA-512 SHA-256 digital signature for the given data.
-
-    Args:
-        data (bytes): The data that was signed
-        signature (bytes): The RSA511 SHA-256 digital signature to be verified
-        modulus (int): the modulus value of the RSA-512
-        exponent (int): the modulus value of the RSA-512
-
-    Returns:
-        bool: True if the signature is valid, False otherwise.
-    """
-    # Construct the public key
-    public_key = rsa.RSAPublicNumbers(exponent, modulus).public_key(default_backend())
-    
-    # Hash the data
-    hasher = hashes.Hash(hashes.SHA256(), default_backend())
-    hasher.update(data)
-    hashed_data = hasher.finalize()
-    expected = hashed_data.hex()
-    # print(f"Hashed Data: {hashed_data.hex()}")
-
-    # Decrypt digital signature (manually do this because public_key.encrypt is
-    # dumb)
-    result = pow(int.from_bytes(signature), exponent, modulus)
-    received = hex(result)[-64:].removeprefix("0x").rjust(64, "0")
-    # print(f"Decoded Public Key Hash: {received}")
-
-    # TODO: investigate why this doesn't work
-    # received = public_key.encrypt(signature, padding=padding.PKCS1v15())
-
-    # Verify the signature
-    try:
-        public_key.verify(
-            signature,
-            data,
-            padding.PKCS1v15(),
-            hashes.SHA256()
-        )
-        return True, received, expected
-    except Exception as e:
-        return False, received, expected
-    
-def xor_decrypt(data: bytes, key: bytes):
-    decrypted_data = bytearray()
-    key_length = len(key)
-
-    for i, byte in enumerate(data):
-        decrypted_data.append(byte ^ key[i % key_length])
-
-    return bytes(decrypted_data)
